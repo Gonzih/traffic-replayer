305ac083c6adff80f8db08ee137885eaf5b96422
Max Gonzih <gonzih@gmail.com>
2016-07-18 12:00:39 +0200

    Bring this codebase a bit closer to the 21 centry

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject traffic-replayer "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :main traffic-replayer)
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main traffic-replayer.core)
